62c7f93bcf7217be3399a6500d2510e0c7f8a18a
Armando Beltran <armandobeltran18@gmail.com>
2023-01-14 22:20:34 -0600

    Defined Logic for auto refresh token via hour interval

--- server/server.js
+++ server/server.js
@@ -9,6 +9,25 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '60e95acabd45469ea7a6e3fb3b1048a8',
+        clientSecret: 'd2a05c2e8fcb432989389afed3c35159',
+        refreshToken,
+    })  
+
+    spotifyApi.refreshAccessToken().then((data) => {
+        res.json({
+            accessToken: data.body.accessToken,
+            expiresIn: data.body.expiresIn
+        })
+    }).catch(() => {
+        res.sendStatus(400) 
+    })
+})
+
 
 app.post('/login', (req, res) => {
     const code = req.body.code
