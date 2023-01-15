65e2a0b251b5fcdd15c348690837247a54096898
Armando Beltran <armandobeltran18@gmail.com>
2023-01-14 23:13:55 -0600

    Started Defining Logic for Artists searching - Currently erroring

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,42 @@
 import useAuth from './useAuth'
+import { Container, Form } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-node'
 
 
 const Dashboard = ({code}) => {
+    const [search, setSearch] = useState("")
+    const [searchResults, setSearchResults] = useState([])
+
     const accessToken = useAuth(code)
+
+    const handleChange = (e) => setSearch(e.target.value);
+
+    const spotifyApi = new SpotifyWebApi({
+        clientId: '60e95acabd45469ea7a6e3fb3b1048a8'
+    })
+
+    useEffect(() => {
+        if(!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken])
+
+
+    useEffect(() => {
+        if(!search) return setSearchResults([])
+        if(!accessToken) return
+
+        spotifyApi.searchTracks(search).then(res => {
+            console.log(res) //Getting errors on searching 
+        })
+    }, [search, accessToken])
+
     return ( 
-        <div>
-            {code}
-        </div>
+        <Container className='d-flex flex-column py-2' style={{height: '100vh'}}>
+            <Form.Control type='search' placeholder='Search Songs/Artists' value={search} onChange={handleChange}/>
+            <div className='flex-grow-1 my-2' style={{ overflowY: 'auto'}}>Songs</div>
+            <div>Bottom</div>
+        </Container>
      );
 }
  
