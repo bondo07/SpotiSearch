3f7746dbd6516afe9dec67ab3e3d4e5ef60bb886
Armando Beltran <armandobeltran18@gmail.com>
2023-02-08 00:07:33 -0600

    Created Logic for API call for Lyrics to be shown on screen + tested the rest of functionality and confirmed working

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,69 +1,105 @@
-import useAuth from './useAuth'
-import { Container, Form } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import SpotifyWebApi from 'spotify-web-api-node'
-import TrackSearchResult from './TrackSearchResult';
-import Player from './Player';
+import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
+import axios from "axios";
 
 const spotifyApi = new SpotifyWebApi({
-    clientId: '0fd5af8b613d4d009f7a6f0f3238a61e',
-})
+  clientId: "0fd5af8b613d4d009f7a6f0f3238a61e",
+});
 
-const Dashboard = ({code}) => {
-    const [search, setSearch] = useState("")
-    const [searchResults, setSearchResults] = useState([])
-    const [playingTrack, setPlayingTrack] = useState()
+const Dashboard = ({ code }) => {
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+  const [showLyrics, setShowLyrics] = useState("");
 
-    function chooseTrack(track) {
-        setPlayingTrack(track)
-        setSearch('')
-    }
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+    setShowLyrics("");
+  }
 
-    const accessToken = useAuth(code)
+  const accessToken = useAuth(code);
 
-    const handleChange = (e) => setSearch(e.target.value);
+  const handleChange = (e) => setSearch(e.target.value);
 
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
 
-    useEffect(() => {
-        if(!accessToken) return
-        spotifyApi.setAccessToken(accessToken)
-    }, [accessToken])
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
 
-
-    useEffect(() => {
-        if(!search) return setSearchResults([])
-        if(!accessToken) return
-
-        let cancel = false
-        spotifyApi.searchTracks(search).then(res => {
-            if(cancel) return
-            setSearchResults(res.body.tracks.items.map(track => {
-                const smallestAlbumImage = track.album.images.reduce((smallest, image) => {
-                    if (image.height < smallest.height) return image
-                    return smallest
-                }, track.album.images[0])
-                return {
-                    artist: track.artists[0].name,
-                    title: track.name,
-                    uri: track.uri,
-                    albumUrl: smallestAlbumImage.url
-                }
-            }))
+    let cancel = false;
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
         })
+      );
+    });
 
-        return () => cancel = true
-    }, [search, accessToken])
+    return () => (cancel = true);
+  }, [search, accessToken]);
 
-    return ( 
-        <Container className='d-flex flex-column py-2' style={{height: '100vh'}}>
-            <Form.Control type='search' placeholder='Search Songs/Artists' value={search} onChange={handleChange}/>
-            <div className='flex-grow-1 my-2' style={{ overflowY: 'auto'}}>
-                {searchResults.map(track => (
-                    <TrackSearchResult track={track} key= {track.uri} chooseTrack={chooseTrack}/>
-            ))}</div>
-            <div><Player accessToken={accessToken} trackUri={playingTrack?.uri}/></div>
-        </Container>
-     );
-}
- 
+  useEffect(() => {
+    if(!playingTrack) return
+
+    axios.get('http://localhost:3001/lyrics', {
+        params: {
+            track: playingTrack.title,
+            artist: playingTrack.artist
+        }
+    }).then(res => {
+        setShowLyrics(res.data.lyrics)
+    })
+  }, [playingTrack])
+
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={handleChange}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map((track) => (
+          <TrackSearchResult
+            track={track}
+            key={track.uri}
+            chooseTrack={chooseTrack}
+          />
+        ))}
+        {searchResults.length === 0 && (
+            <div className="text-center" style={{ whiteSpace: "pre" }}>
+                {showLyrics}
+            </div>
+        )}
+      </div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
+    </Container>
+  );
+};
+
 export default Dashboard;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require("express"); //I dont think the login is being done prope
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
+const lyricsFinder = require("lyrics-finder");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
@@ -52,5 +54,11 @@ app.post("/login", (req, res) => {
       res.sendStatus(400);
     });
 });
+
+app.get('/lyrics', async (req, res) => {
+  const lyrics = (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Available"
+  res.json({ lyrics })
+})
+
 
 app.listen(3001);
